2a472ba476b0ada24fee7a2aad3948b790f41b8b
Pedram Zamirai <pedram.zamiraei@gmail.com>
2016-12-17 19:10:47 +0330

    init

---
+++ algorithm/Hill-climbing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "problem.cpp"
+
+using namespace std;
+
+class algorithm{
+public:
+	problem p;
+};
+
+int main(){
+	node a;
+	a.score = 10;
+	return 0;
+}
---
+++ problem/problem.cpp
@@ -0,0 +1,9 @@
+
+struct node{
+	int score;	
+};
+class problem{
+public:
+	void set_initial();
+	void action(int);
+};
