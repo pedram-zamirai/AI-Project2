5fccc627ecbdd303de25bfd77750d0b08cb81823
Pedram Zamirai <pedram.zamiraei@gmail.com>
2016-12-18 22:10:36 +0330

    hill climb first selection completed

---
+++ algorithm/Hill-climbing-firstselection.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "../problem/8queen.cpp"
+
+using namespace std;
+
+class algorithm{
+public:
+	problem p;
+	node current, next, neighbour;
+	void run();
+};
+
+void algorithm::run(){
+	current = p.set_initialstate();
+	int curr_score;
+	while(true){
+		curr_score = current.score;
+		for(int i=1; p.is_neighbour(i); i++){
+			neighbour = p.neighbour(current, i);
+			if(curr_score < neighbour.score){
+				next = neighbour;
+				curr_score = neighbour.score;
+
+			}
+		}
+		if(current.score == curr_score)
+			break;
+		current = next;
+	}
+	p.print_sol(current);
+}
+
+int main(){
+	algorithm hill;
+	hill.run();
+	return 0;
+}
